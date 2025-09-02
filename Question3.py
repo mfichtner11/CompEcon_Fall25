d636f8ecfb4b2dedc1d7518eb847dab5c811a730
mfichtner11 <marli.fichtner@gmail.com>
2025-09-02 13:07:39 -0400

    Upload PS2

---
+++ Question3.py
@@ -0,0 +1,13 @@
+# Question3_py generation
+
+# q3.py 
+    
+def prod_q3(a, b): 
+    return a*b
+def sum_q3(a, b): 
+    return a+b
+def sqrt_q3(x): 
+    from math import sqrt
+    return sqrt(x)
+
+
